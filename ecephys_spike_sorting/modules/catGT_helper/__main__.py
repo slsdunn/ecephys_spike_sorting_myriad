6e8f415ec115f537ba8f338f969e853841125ffc
slsdunn <soraya.s.dunn@gmail.com>
2023-04-11 17:34:43 +0100

    Revert "Revert "fix typo/bug so works on linux""
    This reverts commit d4ffd9845be2b32e95212763696da25bd6a316ff.

--- ecephys_spike_sorting/modules/catGT_helper/__main__.py
+++ ecephys_spike_sorting/modules/catGT_helper/__main__.py
@@ -21,7 +21,7 @@ def run_CatGT(args):
         # call catGT directly with params. CatGT.log file will be saved lcoally
         # in current working directory (with the calling script)
         catGTexe_fullpath = catGTPath.replace('\\', '/') + "/CatGT"
-    elif sys.platform.starstwith('linux'):
+    elif sys.platform.startswith('linux'):
         catGTexe_fullpath = catGTPath.replace('\\', '/') + "/runit.sh"
     else:
         print('unknown system, cannot run CatGt')
@@ -61,6 +61,8 @@ def run_CatGT(args):
     
     print('CatGT command line:' + catGT_cmd)
     
+    catGT_cmd = catGT_cmd.split(' ') # added by Jules+Soraya on 3/2/23 to work with Linux
+
     start = time.time()
     subprocess.call(catGT_cmd)
 
